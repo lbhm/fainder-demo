d5c5d428a42cbac6a9d4aed8b64a875657f8b489
Leonard Geißler <60317151+GodBambusleitung@users.noreply.github.com>
2025-01-28 15:59:48 +0100

    add hand to logo hover

--- ui/app.vue
+++ ui/app.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-app-bar :elevation="0" height="85">
-      <Logo size="medium" class="mr-4 ml-4" @click="gotoHome"/>
+      <Logo size="medium" class="mr-4 ml-4 clickable" @click="gotoHome"/>
       <v-spacer></v-spacer>
 
       <!-- Add search component in app bar only on results page -->
@@ -184,5 +184,9 @@
 
 .app-bar-search :deep(.v-field) {
   border-radius: 20px;
+}
+
+.clickable {
+  cursor: pointer;
 }
 </style>
